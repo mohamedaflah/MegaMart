06c585b0bd75ceaf221209e1d0506ec1b0ae0b1c
Aflu <mohamedaflah186@gmail.com>
2023-11-25 15:18:00 +0530

    _

--- public/javascript/productlist.js
+++ public/javascript/productlist.js
@@ -41,7 +41,7 @@ rangeInput.forEach((input) => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  let data=localStorage.getItem("filterData")
+  let data = localStorage.getItem("filterData");
   // let checkedCategories=localStorage.getItem("filtercategories")
   // let checkedBrands=localStorage.getItem("filterbrands")
   // if(checkedCategories ){
@@ -67,20 +67,18 @@ document.addEventListener("DOMContentLoaded", () => {
   //   }
   // }
 
-  
-  if(data){
-    document.getElementById("displayData").innerHTML=data
-
+  if (data) {
+    document.getElementById("displayData").innerHTML = data;
   }
   const updateProductList = () => {
     const categories = Array.from(
       document.querySelectorAll('input[name="category"]:checked')
     ).map((checkbox) => checkbox.value);
-    localStorage.setItem("filtercategories",categories) 
+    localStorage.setItem("filtercategories", categories);
     const brands = Array.from(
       document.querySelectorAll('input[name="brand"]:checked')
-      ).map((checkbox) => checkbox.value);
-      localStorage.setItem("filterbrands",brands)
+    ).map((checkbox) => checkbox.value);
+    localStorage.setItem("filterbrands", brands);
     // const sortedByPrice=document.querySelectorAll('input[name="sortprice"]:checked')?.value || '';
     const sortedBy =
       document.querySelector('input[name="sort"]:checked')?.value || "";
@@ -89,6 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //   brand=${brands}
     //   sort=${sortedBy}
     //   `);
+    let allCard = [];
     fetch(
       `/users/product/filtereandsort/?category=${categories.join(
         ","
@@ -96,59 +95,69 @@ document.addEventListener("DOMContentLoaded", () => {
     )
       .then((response) => response.json())
       .then((res) => {
-        console.log(JSON.stringify(res.data));
-        let allCard = [];
         const id = localStorage.getItem("userId");
         res.data.forEach((product) => {
           if (!product.deletionStatus) {
-
-            let card = `<a class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6  d-flex justify-content-center text-decoration-none text-dark" onclick="window.location.href='/products/product-detail/${product._id}/mainimage'">
+            let card = `<a class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6  d-flex justify-content-center text-decoration-none text-dark" onclick="window.location.href='/products/product-detail/${
+              product._id
+            }/mainimage'">
             <div class="card d-flex flex-column justify-content-between position-relative" style="width: 12rem;height: 15rem;box-shadow: 0px 1px 13px 0px rgba(0, 0, 0, 0.156);border: none;">
-                ${product.stock < 1
+                ${
+                  product.stock < 1
                     ? `<span style="font-size: 12px;padding:2px;background: rgba(255, 0, 0, 0.224);width: 90px;margin: 2px;border-radius: 30px;display: flex;align-items: center;justify-content: center;" class="text-white">out of stock</span>`
                     : `<span style="visibility:hidden;font-size: 12px;padding:2px;background: rgba(255, 0, 0, 0.224);width: 90px;margin: 2px;border-radius: 30px;display: flex;align-items: center;justify-content: center;" class="text-white"></span>`
                 }
                 <div class="img d-flex justify-content-center" style="border-bottom: 1px solid rgb(205, 205, 205);height: 6rem;">
-                    <img src="/product-images/${product.image[0].mainimage}" style="height: 80%;width: auto;" alt="" class="card-top-img mt-2">
+                    <img src="/product-images/${
+                      product.image[0].mainimage
+                    }" style="height: 80%;width: auto;" alt="" class="card-top-img mt-2">
                 </div>
                 <div class="px-2">
                     <span style="font-size: 13px;">${product.productName}</span>
                 </div>
                 <div class="px-2 d-flex justify-content-between">
                     <span style="font-size: 13px;">
-                        ${product.discount ? `₹${product.discount}` : `₹${product.price}`}
+                        ${
+                          product.discount
+                            ? `₹${product.discount}`
+                            : `₹${product.price}`
+                        }
                     </span>
                     <span style="font-size: 13px;text-decoration: line-through;">
                         ${product.discount ? `₹${product.price}` : ""}
                     </span>
                 </div>
                 <div class="px-2 d-flex justify-content-between mb-2">
-                    ${product.stock < 1
+                    ${
+                      product.stock < 1
                         ? `<button class="btn-dark m-0 py-1 px-3 disabled" style="font-size: 12px;background: rgba(107, 105, 105, 0.818);cursor: no-drop;" disabled>Add to cart</button>`
-                        : `${id
-                            ? `${product && product.cartExist
-                                ? `<button class="btn-dark m-0 py-1 px-3" style="font-size: 12px;" onclick="GotocartinProduct(event)">Go to cart</button>`
-                                : `<button class="btn-dark m-0 py-1 px-3" style="font-size: 12px;" onclick="addToCart(event,'${product._id}','products','${id}','changeBtn${product._id}')" id="changeBtn${product._id}">Add to cart</button>`
-                            }`
-                            : `<button class="btn-dark m-0 py-1 px-3" style="font-size: 12px;">Add to cart</button>`
-                        }`
+                        : `${
+                            id
+                              ? `${
+                                  product && product.cartExist
+                                    ? `<button class="btn-dark m-0 py-1 px-3" style="font-size: 12px;" onclick="GotocartinProduct(event)">Go to cart</button>`
+                                    : `<button class="btn-dark m-0 py-1 px-3" style="font-size: 12px;" onclick="addToCart(event,'${product._id}','products','${id}','changeBtn${product._id}')" id="changeBtn${product._id}">Add to cart</button>`
+                                }`
+                              : `<button class="btn-dark m-0 py-1 px-3" style="font-size: 12px;">Add to cart</button>`
+                          }`
                     }
-                    ${id
-                        ? `${product && product.whishExist
-                            ? `<button class="btn-dark m-0 py-1 px-2" style="font-size: 15px;box-shadow: 0px 1px 13px 0px rgba(0, 0, 0, 0.179);color: #e81224;background: transparent;" onclick="addToWhishListinProducts(event,'${product._id}','${id}',this,'remove')"><i class="fa fa-heart"></i></button>`
-                            : `<button class="btn-dark m-0 py-1 px-2" style="font-size: 15px;" onclick="addToWhishListinProducts(event,'${product._id}','${id}',this,'add')"><i class="fa fa-heart"></i></button>`
-                        }`
+                    ${
+                      id
+                        ? `${
+                            product && product.whishExist
+                              ? `<button class="btn-dark m-0 py-1 px-2" style="font-size: 15px;box-shadow: 0px 1px 13px 0px rgba(0, 0, 0, 0.179);color: #e81224;background: transparent;" onclick="addToWhishListinProducts(event,'${product._id}','${id}',this,'remove')"><i class="fa fa-heart"></i></button>`
+                              : `<button class="btn-dark m-0 py-1 px-2" style="font-size: 15px;" onclick="addToWhishListinProducts(event,'${product._id}','${id}',this,'add')"><i class="fa fa-heart"></i></button>`
+                          }`
                         : `<button class="btn-dark m-0 py-1 px-2" style="font-size: 15px;" onclick="window.location.href='/user/login'"><i class="fa fa-heart"></i></button>`
                     }
                 </div>
             </div>
         </a>`;
-        ;
             allCard.push(card);
           }
         });
         const cardConverted = allCard.join("");
-        localStorage.setItem("filterData",cardConverted)
+        localStorage.setItem("filterData", cardConverted);
         if (cardConverted.length > 0) {
           document.getElementById("displayData").innerHTML = cardConverted;
         } else {
@@ -183,7 +192,10 @@ async function addToWhishListinProducts(event, productId, userId, btn, doing) {
       btn.style.color = "#e81224";
       btn.style.boxShadow = "0px 1px 13px 0px rgba(0, 0, 0, 0.179)";
       btn.style.background = "transparent";
-      btn.setAttribute("onclick",`addToWhishListinProducts(event, ${productId}, ${userId}, this, 'remove')`)
+      btn.setAttribute(
+        "onclick",
+        `addToWhishListinProducts(event, ${productId}, ${userId}, this, 'remove')`
+      );
       let ct = document.getElementById("whish");
       let current = ct.textContent;
       ct.textContent = Number(current) + 1;
@@ -191,20 +203,25 @@ async function addToWhishListinProducts(event, productId, userId, btn, doing) {
   } else {
     fetch(
       `/users/product/whishlist/remove-product-whish/${productId}/${userId}`
-      ).then((res) => {
+    )
+      .then((res) => {
         return res.json();
-      }).then(res=>{
-      if(res.status){
-      btn.setAttribute("onclick",`addToWhishListinProducts(event,${productId},${userId},this,'add')`)
-        btn.style.boxShadow=''
-        btn.style.background='#212529'
-        btn.style.color='white'
-        let ct = document.getElementById("whish");
-        let current = ct.textContent;
-        ct.textContent = Number(current) - 1;
-        let whiShCurr = Number(whishCountinWhish.textContent);
-        whishCountinWhish.textContent = whiShCurr - 1;
-      }
-    })
+      })
+      .then((res) => {
+        if (res.status) {
+          btn.setAttribute(
+            "onclick",
+            `addToWhishListinProducts(event,${productId},${userId},this,'add')`
+          );
+          btn.style.boxShadow = "";
+          btn.style.background = "#212529";
+          btn.style.color = "white";
+          let ct = document.getElementById("whish");
+          let current = ct.textContent;
+          ct.textContent = Number(current) - 1;
+          let whiShCurr = Number(whishCountinWhish.textContent);
+          whishCountinWhish.textContent = whiShCurr - 1;
+        }
+      });
   }
 }
