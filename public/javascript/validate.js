3e39e921e9e2d6891ddfc8810a1d4b3f1dc3c21d
Aflu <mohamedaflah186@gmail.com>
2024-02-09 15:25:10 +0530

    codes updated

--- public/javascript/validate.js
+++ public/javascript/validate.js
@@ -1,12 +1,4 @@
-// function handleResponse(id,productid,event){
-//     event.preventDefault()
-//     fetch(`/users/product/cart/increaseqty/${id}/${productid}/`).then((response)=>{
-//         return response.json()
-//     }).then((data)=>{
-//         console.log(data.status)
-//     })
-//     event.preventDefault()
-// }
+
 let but = document.querySelector("#profile_btn");
 
 but.addEventListener("click", () => {
@@ -28,33 +20,3 @@ function displayProfileImage(inputDom,imgDom){
       img.src=''
   }
 }
-// async function changeQty(user_id, product_id, qty,changeType) {
-//   try {
-//     if(changeType=='increase'){
-//       const response = await fetch(`/users/product/cart/increaseqty/${user_id}/${product_id}`);
-//       if (response.ok) {
-//         const qtyDisplay = document.getElementById("qty_display");
-//         const currentQty = parseInt(qtyDisplay.textContent, 10) || 0;
-//         const newQty = currentQty + 1;
-//         qtyDisplay.textContent = newQty < 10 ? `0${newQty}` : newQty.toString();
-//       }
-//     }else if(changeType=='decrease'){
-//       let invisible=document.getElementById('visible')
-//       // if(Number(qty)<=1){
-//       //   invisible.style.visibility="hidden"
-//       //   return
-//       // }else{
-//       //   invisible.style.visibility="visible"
-//       // }
-//       const response=await fetch(`/users/product/cart/decreaseqty/${user_id}/${product_id}/`)
-//       if(response.ok){
-//         const qtyDisplay = document.getElementById("qty_display");
-//         const currentQty = parseInt(qtyDisplay.textContent, 10) || 0;
-//         const newQty = currentQty - 1;
-//         qtyDisplay.textContent = newQty < 10 ? `0${newQty}` : newQty.toString();
-//       }
-//     }
-//   } catch (error) {
-//     console.error('Error:', error);
-//   }
-// }
